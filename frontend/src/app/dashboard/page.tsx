4e41b0091bacd365a62f9f5675b80f2da04ca6c4
Pierre Paulo <pierresantista@hotmail.com>
2025-12-16 15:46:50 -0300

    feat: add user-aware redirects for auth pages and scaffold dashboard

---
+++ frontend/src/app/dashboard/page.tsx
@@ -0,0 +1,7 @@
+export default function Dashboard() {
+  return (
+    <div className="">
+      <h1>teste</h1>
+    </div>
+  );
+}
--- frontend/src/lib/auth.ts
+++ frontend/src/lib/auth.ts
@@ -1,4 +1,6 @@
 import { cookies } from "next/headers";
+import { apiClient } from "./api";
+import { User } from "./types";
 
 const COOKIE_NAME = "token_cantina";
 
@@ -21,4 +23,22 @@ export async function setToken(token: string) {
 export async function removeToken() {
   const cookieStore = await cookies();
   cookieStore.delete(COOKIE_NAME);
+}
+
+export async function getUser(): Promise<User | null> {
+  try {
+    const token = await getToken();
+
+    if (!token) {
+      return null;
+    }
+
+    const user = await apiClient<User>("/me", {
+      token: token,
+    });
+
+    return user;
+  } catch (err) {
+    return null;
+  }
 }
