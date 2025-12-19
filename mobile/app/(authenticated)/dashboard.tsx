24653e8f7baf10a0c57ddde6ae40b749b1e695a0
Pierre Paulo <pierresantista@hotmail.com>
2025-12-19 15:54:04 -0300

    feat(mobile): scaffold waiter app theme and layouts

---
+++ mobile/app/(authenticated)/dashboard.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Dashboard() {
+  return (
+    <View>
+      <Text>Pagina Dashboard</Text>
+    </View>
+  );
+}
---
+++ mobile/constants/theme.ts
@@ -0,0 +1,32 @@
+export const colors = {
+  background: "#151A26",
+  backgroundInput: "#0D111B",
+  primary: "#F8F8F8",
+  brand: "#F97316",
+  red: "#f92516ff",
+  borderColor: "#2E333D",
+  green: "#3fff49ff",
+  gray: "#8A8A8A",
+};
+
+export const spacing = {
+  xs: 4,
+  sm: 8,
+  md: 16,
+  lg: 24,
+  xl: 32,
+};
+
+export const borderRadius = {
+  sm: 4,
+  md: 8,
+  lg: 12,
+};
+
+export const fontSize = {
+  sm: 12,
+  md: 14,
+  lg: 16,
+  xl: 20,
+  xxl: 24,
+};
