b33ac18af4a4523246111e164bb3299f26331439
Pierre Paulo <pierresantista@hotmail.com>
2025-12-24 11:25:54 -0300

    feat(mobile): build order screen with category selection

--- mobile/app/(authenticated)/order.tsx
+++ mobile/app/(authenticated)/order.tsx
@@ -1,9 +1,123 @@
-import { Text, View } from "react-native";
+import { Select } from "@/components/Select";
+import { colors, fontSize, spacing } from "@/constants/theme";
+import api from "@/services/api";
+import { Category } from "@/types";
+import { Ionicons } from "@expo/vector-icons";
+import { useLocalSearchParams, useRouter } from "expo-router";
+import { useEffect, useState } from "react";
+import {
+  ActivityIndicator,
+  Pressable,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 export default function Order() {
+  const router = useRouter();
+  const insets = useSafeAreaInsets();
+  const { order_id, table } = useLocalSearchParams<{
+    order_id: string;
+    table: string;
+  }>();
+
+  const [categories, setCategories] = useState<Category[]>([]);
+  const [selectedCategory, setSelectedCategory] = useState("");
+
+  const [loadingCategories, setLoadingCategories] = useState(true);
+
+  useEffect(() => {
+    async function loadDataCategories() {
+      await loadCategories();
+    }
+
+    loadDataCategories();
+  }, []);
+
+  async function loadCategories() {
+    try {
+      const response = await api.get<Category[]>("/category");
+      console.log(response.data);
+      setCategories(response.data);
+    } catch (err) {
+      console.log(err);
+    } finally {
+      setLoadingCategories(false);
+    }
+  }
+
+  if (loadingCategories) {
+    return (
+      <View style={styles.loadingContainer}>
+        <ActivityIndicator size="large" color={colors.brand} />
+      </View>
+    );
+  }
+
   return (
-    <View>
-      <Text>TESTE PAGINA ORDER</Text>
+    <View style={styles.container}>
+      <View style={[styles.header, { paddingTop: insets.top + 8 }]}>
+        <View style={styles.headerContent}>
+          <Text style={styles.headerTitle}>Mesa {table}</Text>
+
+          <Pressable style={styles.closeButton} onPress={() => router.back()}>
+            <Ionicons name="trash" size={20} color={colors.primary} />
+          </Pressable>
+        </View>
+      </View>
+
+      <ScrollView style={styles.scrollContent}>
+        <Select
+          label="Categorias"
+          placeholder="Selecione a categoria..."
+          options={categories.map((category) => ({
+            label: category.name,
+            value: category.id,
+          }))}
+          selectedValue={selectedCategory}
+          onValueChange={setSelectedCategory}
+        />
+      </ScrollView>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  loadingContainer: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: colors.background,
+  },
+  container: {
+    flex: 1,
+    backgroundColor: colors.background,
+  },
+  header: {
+    backgroundColor: colors.background,
+    borderBottomWidth: 1,
+    borderBottomColor: colors.borderColor,
+    paddingBottom: spacing.lg,
+    paddingHorizontal: spacing.lg,
+  },
+  headerContent: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  headerTitle: {
+    fontSize: fontSize.xl,
+    color: colors.primary,
+    fontWeight: "bold",
+  },
+  closeButton: {
+    backgroundColor: colors.red,
+    padding: spacing.sm,
+    borderRadius: 8,
+  },
+  scrollContent: {
+    padding: spacing.lg,
+  },
+});
